62fd471a5e4634ded2f11a532aa075a968c3ab4e
Jeremy Spencer <jls1923@jagmail.southalabama.edu>
2024-04-16 20:21:09 -0500

    commiting changes so I can merge

--- PairedProgrammingProject/src/main/java/com/revature/models/Book.java
+++ PairedProgrammingProject/src/main/java/com/revature/models/Book.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "books")
 @Component
